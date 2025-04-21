4939b8bf987939127d594fe8710d791e0cdbc6b5
Trevor Lachman <tal225@lehigh.edu>
2025-04-21 16:43:13 -0400

    packages needed for auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: "/login", destination: "/pages/login" },
+      { source: "/signup", destination: "/pages/signup" },
+      { source: "/favorites", destination: "/pages/favorites" },
+      { source: "/my-recipes", destination: "/pages/my-recipes" },
+      { source: "/recipe/:id", destination: "/pages/recipe/:id" },
+    ];
+  },
+};
 
 export default nextConfig;
