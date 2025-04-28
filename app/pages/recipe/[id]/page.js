2b4d021fab0501e487cc627c0a8a05e706a34f65
Trevor Lachman <tal225@lehigh.edu>
2025-04-27 22:20:02 -0400

    switch from png to pdf

--- app/pages/recipe/[id]/page.js
+++ app/pages/recipe/[id]/page.js
@@ -8,6 +8,8 @@ import Navbar from "../../../components/Navbar";
 import Loader from "../../../components/Loader";
 import { useAuth } from "../../../context/authContext";
 import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 
 export default function RecipePage() {
@@ -89,12 +91,18 @@ export default function RecipePage() {
       backgroundColor: "#ffffff",
       useCORS: true,
     });
-    const link = document.createElement("a");
-    link.download = `${recipe.title}.png`;
-    link.href = canvas.toDataURL();
-    link.click();
-  };
 
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: [canvas.width, canvas.height],
+    });
+
+    pdf.addImage(imgData, "PNG", 0, 0, canvas.width, canvas.height);
+    pdf.save(`${recipe.title}.pdf`);
+  };
   if (loading) {
     return <Loader />;
   }
